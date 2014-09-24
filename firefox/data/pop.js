0045d6915a5033009a66b5c070bd10834a52a3e0
Billen, Charles <charles.billen@sage.com>
2014-09-24 13:16:55 +0100

    Adds firefox version

---
+++ firefox/data/pop.js
@@ -0,0 +1,13 @@
+(function () {
+    var debits = document.querySelectorAll('input[name*=debit]'),
+        credits = document.querySelectorAll('input[name*=credit]'),
+        l = debits.length - 1;
+
+    for (var i = 0; i < l; i++) {
+        debits[i].value = 1;
+    }
+
+    credits[l].value = l;
+
+    window.scrollTo(0, document.body.scrollHeight);
+})();

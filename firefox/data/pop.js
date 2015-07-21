17b69d4b9dacbbf02a4f39c1e192555be480a635
Billen, Charles <charles.billen@sage.com>
2015-07-21 13:01:00 +0100

    Makes change event creation a little simpler

--- firefox/data/pop.js
+++ firefox/data/pop.js
@@ -1,13 +1,11 @@
 (function () {
     var debits = document.querySelectorAll('input[name*=debit]'),
         credits = document.querySelectorAll('input[name*=credit]'),
-        evnt = document.createEvent('HTMLEvents'),
+        evnt = new Event('change'),
         l = debits.length - 1,
         lastCredit = credits[l],
         total = 0,
         value = 0;
-
-    evnt.initEvent('change', true, true);
 
     for (var i = 0; i < l; i++) {
         value = Math.floor(Math.random() * 1000 + 1);

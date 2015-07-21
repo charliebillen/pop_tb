822b9edcfe5cd58876d3a4ebaa655739dd764d65
Billen, Charles <charles.billen@sage.com>
2015-07-21 12:17:50 +0100

    Triggers total creation

--- chrome/pop.js
+++ chrome/pop.js
@@ -1,9 +1,13 @@
 (function () {
     var debits = document.querySelectorAll('input[name*=debit]'),
         credits = document.querySelectorAll('input[name*=credit]'),
+        evnt = document.createEvent('HTMLEvents'),
         l = debits.length - 1,
+        lastCredit = credits[l],
         total = 0,
         value = 0;
+
+    evnt.initEvent('change', true, true);
 
     for (var i = 0; i < l; i++) {
         value = Math.floor(Math.random() * 1000 + 1);
@@ -11,7 +15,8 @@
         debits[i].value = value;
     }
 
-    credits[l].value = total;
+    lastCredit.value = total;
+    lastCredit.dispatchEvent(evnt);
 
     window.scrollTo(0, document.body.scrollHeight);
 })();
